e9477cfe5cdc075d4f9fa7ec7d0a57ae7542e070
njl <njl@patac.com>
2018-11-20 09:25:19 +0800

    添加了大数据量的测试发现了时间复杂度 的说明是对的

--- TwoSum_leetcode001.cpp
+++ TwoSum_leetcode001.cpp
@@ -18,6 +18,15 @@ return [0, 1].
 #include <chrono>
 using namespace std::chrono;
 using namespace std;
+
+/*
+Time complexity : O(n^2). For each element, we try to find its complement 
+by looping through the rest of array which takes O(n)O(n) time. 
+Therefore, the time complexity is O(n^2)
+
+Space complexity : O(1). 
+
+*/
 class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
@@ -48,6 +57,16 @@ private:
 	vector<int> result;
 };
 
+/*
+Complexity Analysis:
+
+Time complexity : O(n). We traverse the list containing nn elements exactly twice. 
+Since the hash table reduces the look up time to O(1), the time complexity is O(n).
+
+Space complexity : O(n). The extra space required depends on the number of items stored in the hash table, 
+which stores exactly nn elements. 
+
+*/
 class Solution1 {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
@@ -72,6 +91,38 @@ public:
 		return result;
 	}
 };
+
+class Solution2 {
+public:
+    int findY(int x, int target){
+        if(x == 0) return target;
+        if(target == 0) return -x;
+        else{
+            if(x == target)
+                return 0;
+            else
+                return -x + target;
+        }
+    }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        unordered_map<int, int> bank;
+        int find = 0;
+        for(int i = 0; i < nums.size(); i++){
+            find = findY(nums[i], target);
+            if(bank.find(find) != bank.end()){
+                result.push_back(i);
+                result.push_back(bank[find]);
+            return result;
+            }else{
+                if(bank.find(nums[i]) == bank.end())
+                    bank.insert({nums[i],i});
+            }
+        }
+        return result;
+    }
+};
+
 
 /*
 Solution mys;
@@ -98,14 +149,48 @@ result[0] is 1
 result[1] is 7
 
 为什么hash时间更长
+以下是50万个元素的测试数据
+Solution2 mys;
+
+250.164
+Running time is: 252.498ms
+result[0] is 499996
+result[1] is 499994
+
+Solution1 mys;
+
+226.121
+Running time is: 228.478ms
+result[0] is 499994
+result[1] is 499996
+^C
+njl@njl-HP:~/Documents/test$ ./a.out 
+226.451
+Running time is: 229.755ms
+result[0] is 499994
+result[1] is 499996
+
+Solution mys;
+897492 竟然需要大概15分钟
+Running time is: 897455ms
+result[0] is 499992
+result[1] is 499998
+
 */
 int main(int argc, char * * argv)
 {
-	int test[] = { 2, 7, 11, 15 ,1,3,5,8,10};
+    int test[500000] = {0};
+
+for(int i =0;i<500000;i++)
+{
+    test[i] = i;
+    //cout<<i<<endl;
+}
+	
 	vector<int> result1(2, -1);
-	vector<int> nums(test, test + 9);
-	int target = 15;
-	Solution mys;
+	vector<int> nums(test, test + 499999);
+	int target = 999990;
+	Solution1 mys;
 	vector<int> result;
 	clock_t start_time = clock();
 	high_resolution_clock::time_point t1 = high_resolution_clock::now(); //返回时间戳
